05179cc911437f1c101951629db0f45641969929
Frederick John <Freddy@Fredericks-iMac.local>
2017-05-12 03:35:50 -0400

    development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,18 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+
+    config.action_mailer.smtp_settings = {
+    user_name:      ENV['SENDMAIL_USERNAME'],
+    password:       ENV['SENDMAIL_PASSWORD'],
+    domain:         ENV['MAIL_HOST'],
+    address:       'mail.cryptom.space',
+    port:          '465',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
