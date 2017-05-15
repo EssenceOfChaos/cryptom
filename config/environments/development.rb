d4d6bb419cc83e5740e5acd596332055eee9f827
Frederick John <Freddy@Fredericks-iMac.local>
2017-05-14 21:05:51 -0400

    fixed error with email confirmations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,22 +47,21 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-# config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
 # config.action_mailer.smtp_settings = {:address => "localhost", :port => 3000}
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:      ENV['SENDMAIL_USERNAME'],
-    password:       ENV['SENDMAIL_PASSWORD'],
-    domain:         ENV['MAIL_HOST'],
-    address:       'mail.cryptom.space',
-    port:          '465',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => ENV['gmail_username'],
+ :password             => ENV['gmail_password'],
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,16 +75,15 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-
-    config.action_mailer.smtp_settings = {
-    user_name:      ENV['SENDMAIL_USERNAME'],
-    password:       ENV['SENDMAIL_PASSWORD'],
-    domain:         ENV['MAIL_HOST'],
-    address:       'mail.cryptom.space',
-    port:          '465',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => ENV['gmail_username'],
+ :password             => ENV['gmail_password'],
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
 
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
