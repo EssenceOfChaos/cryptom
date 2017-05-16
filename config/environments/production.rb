2c7c8b588953af5af182116b71ef022642e873a0
Frederick John <Freddy@Fredericks-iMac.local>
2017-05-16 09:53:51 -0400

    fix sending emails in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,8 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
 config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+config.action_mailer.perform_deliveries = true
+config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
